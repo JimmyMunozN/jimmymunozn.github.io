1555cf3e61ec16d3cc035922e075d57af22909c3
JimmyMunozN <jimmymunozn@gmail.com>
2026-02-18 10:32:09 -0500

    Discard freezing setup

--- redirect.py
+++ redirect.py
@@ -1,7 +1,5 @@
 from flask import Flask, render_template, send_from_directory
-from flask_frozen import Freezer
 import os
-import sys
 from dotenv import load_dotenv
 
 if os.path.exists('/etc/secrets/.env'):
@@ -14,25 +12,23 @@ app = Flask(__name__,
             static_url_path='/static',
             template_folder='templates')
 
-app.config['FREEZER_RELATIVE_URLS'] = True
-app.config['FREEZER_DESTINATION_IGNORE'] = ['.git*', 'CNAME']
-
-
 @app.route('/')
 def inicio():
     return render_template('main.html', initial_content_template='start.html')
 
 @app.route('/get-content/<page_name>')
 def get_content(page_name):
-    paginas_validas = ['start', 'home', 'about', 'projects', 'contact']
-    if page_name in paginas_validas:
+    if page_name in ['start', 'home', 'about', 'projects', 'contact']:
         return render_template(f'{page_name}.html')
     return "Contenido no encontrado", 404
 
+
 @app.route('/downloadCV')
 def downloadCV():
-    directory = os.path.join(app.root_path, 'static', 'downloads')
+    directory = app.root_path + '/static/downloads/'
+
     filename = 'JimmyMunoz_CV.pdf'
+    
     download_name = 'CV_JimmyMuñoz_Junior-FullStack.pdf'
 
     return send_from_directory(
@@ -42,19 +38,7 @@ def downloadCV():
         download_name=download_name
     )
 
-
-freezer = Freezer(app)
-
-@freezer.register_generator
-def get_content_generator():
-    for page in ['start', 'home', 'about', 'projects', 'contact']:
-        yield 'get_content', {'page_name': page}
-
+app = app
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1 and sys.argv[1] == 'freeze':
-        print("Iniciando proceso de congelado...")
-        freezer.freeze()
-        print("¡Completado! Los archivos listos para GitHub están en la carpeta 'build/'.")
-    else:
-        app.run(debug=True)
+    app.run(debug=True)
